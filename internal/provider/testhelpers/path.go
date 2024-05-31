3be8099be8a2291098fc7d8eefdcc38fa7135779
jakemalachowski <jake@render.com>
2024-05-31 10:44:53 -0500

    Update example path function to reflect repository name update

--- internal/provider/testhelpers/path.go
+++ internal/provider/testhelpers/path.go
@@ -9,16 +9,18 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const parentDirectory = "terraform-provider-render"
+
 func ExamplesPath(t *testing.T) string {
 	dir, err := os.Getwd()
 	require.NoError(t, err)
 
-	index := strings.LastIndex(dir, "terraform-provider")
+	index := strings.LastIndex(dir, parentDirectory)
 	if index == -1 {
-		t.Fatalf("could not find 'terraform-provider' in the path")
+		t.Fatalf("could not find '%s' in the path", parentDirectory)
 	}
 
-	parentDir := dir[:index+len("terraform-provider")]
+	parentDir := dir[:index+len(parentDirectory)]
 	dir = path.Join(parentDir, "examples")
 	return dir
 }
