7622b123b13652fa05679238d8532d75861cfb6c
scottnuma <scottnuma@users.noreply.github.com>
2024-11-18 13:40:15 -0800

    Update postgres datasource test

--- internal/provider/postgres/datasource/testdata/postgres.tf
+++ internal/provider/postgres/datasource/testdata/postgres.tf
@@ -3,7 +3,8 @@ resource "render_postgres" "test" {
   database_name = "test_name_mnop"
   database_user = "test_user"
   high_availability_enabled = false
-  plan = "starter"
+  plan = "basic_256mb"
+  disk_size_gb = 20
   region = "oregon"
   version = "16"
 }
